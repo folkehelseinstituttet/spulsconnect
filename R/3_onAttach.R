64375d86d2a07c892670aa35142480c0b638b3d1
Travis CI User <travis@example.org>
2020-01-07 12:44:13 +0000

    Travis CI update 6 [ci skip]

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\nPACKAGE: spulsconnect")
-  packageStartupMessage("Version: 2020.01.07 at 10:12")
+  packageStartupMessage("Version: 2020.01.07 at 12:32")
   packageStartupMessage("\nIf you have any technical problems, please contact:")
   packageStartupMessage("- RichardAubrey.White@fhi.no")
   packageStartupMessage("- GunnarOyvindIsaksson.Ro@fhi.no")
